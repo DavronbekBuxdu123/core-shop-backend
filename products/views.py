91f4d082ac67b4b9b85c1cdf1d41ed9d21524118
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-10 05:57:16 +0500

    feat: add view logic part

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Category,Product
+from .serializers import CategorySerializer,ProductSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset=Category.objects.filter(parent__isnull=True)
+    serializer_class=CategorySerializer
+    permission_classes=AllowAny
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()    
+    serializer_class=ProductSerializer
+    permission_classes=AllowAny
